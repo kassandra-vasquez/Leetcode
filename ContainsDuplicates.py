a0128cca739d67b0de9953c6e34c2e02aeabd976
kassandra <kyanelyv@yahoo.com>
2024-05-24 23:14:11 -0700

    contains duplicate brute force/optimized solutions

---
+++ ContainsDuplicates.py
@@ -0,0 +1,19 @@
+# BRUTE FORCE
+# TC: T: O(n^2) - quadratic/worst S: O(1) - constant/best
+def hasDuplicates(self, nums: List[int]) -> bool:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+# OPTIMIZED SOLUTION
+# TC: T: O(n) - constant/best S: O(n) - constant/best
+def hasDuplicate(self, nums: List[int]) -> bool:
+    hashset = set()
+
+    for num in nums:
+        if num in hashset:
+            return True
+        hashset.add(num)
+    return False
