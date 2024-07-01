5b6e694f56400c1bf37ca36bf4bc8d12ff7088d2
kassandra <kyanelyv@yahoo.com>
2024-07-01 07:56:03 -0700

    small formatting changes

--- KthLargestElementInAStream.py
+++ KthLargestElementInAStream.py
@@ -1,7 +1,6 @@
 # BRUTE FORCE
 # TC: T: O(n log n) S: O(n)
 class KthLargest:
-
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
@@ -14,13 +13,13 @@ class KthLargest:
 # OPTIMIZED
 # TC: T: O(n log n) S: O(n)
 class KthLargest:
-
     def __init__(self, k: int, nums: List[int]):
         self.minHeap, self.k = nums, k
         heapq.heapify(self.minHeap)
 
         while len(self.minHeap) > k:
             heapq.heappop(self.minHeap)
+
 
     def add(self, val: int) -> int:
         heapq.heappush(self.minHeap, val)
