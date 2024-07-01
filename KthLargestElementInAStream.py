cde1bc2f79efa17dd9c0e55abb69be22963ef098
kassandra <kyanelyv@yahoo.com>
2024-07-01 07:36:00 -0700

    heap/ kth largest int in a stream

---
+++ KthLargestElementInAStream.py
@@ -0,0 +1,30 @@
+# BRUTE FORCE
+# TC: T: O(n log n) S: O(n)
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort()
+        return self.nums[-self.k]
+
+# OPTIMIZED
+# TC: T: O(n log n) S: O(n)
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
