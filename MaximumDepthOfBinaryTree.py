2343fdf273d2594f38e9d0f4a31f3b17bda0e098
kassandra-vasquez <kyanelyyy@gmail.com>
2024-07-18 19:17:18 -0700

    added itreative method max depth binary tree

--- MaximumDepthOfBinaryTree.py
+++ MaximumDepthOfBinaryTree.py
@@ -6,4 +6,19 @@ class Solution:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-    
+
+# ITERATIVE DFS
+# TC: T: O(n) S: O(h)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
