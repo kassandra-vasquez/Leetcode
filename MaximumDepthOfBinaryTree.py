4a371e9195701b472da1651312b83866e44be08f
kassandra-vasquez <kyanelyyy@gmail.com>
2024-07-21 14:35:01 -0700

    added BFS max depth of binary tree solution

--- MaximumDepthOfBinaryTree.py
+++ MaximumDepthOfBinaryTree.py
@@ -22,3 +22,25 @@ class Solution:
                 stack.append([node.left, depth + 1])
                 stack.append([node.right, depth + 1])
         return res
+
+# BFS
+# TC: T: O(n) S: O(m) - max number of nodes at any level
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+
+        if root:
+            q.append(root)
+
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
