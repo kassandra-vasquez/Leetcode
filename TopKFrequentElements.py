61c4e91f867bca24c75069d4dc51650a774c4762
kassandra-vasquez <kyanelyyy@gmail.com>
2024-07-30 07:45:00 -0700

    added optimized top k freq elemeents solution

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -10,3 +10,21 @@ class Solution:
             if len(min_heap) > k:
                 heappop(min_heap)
         return [num for (counts, num) in min_heap]
+
+# OPTIMMIZED
+# TC: T: O(n log k) S: O(n + k)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = defaultdict(int)
+
+        for n in nums:
+            freq[n] += 1
+
+        heap = []
+
+        for key, count in freq.items():
+            if len(heap) == k:
+                heappushpop(heap, (key, count))
+            else:
+                heappush(heap, (key, count))
+        return [item[1] for item in heap]
