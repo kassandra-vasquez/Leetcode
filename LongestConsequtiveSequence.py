a1b6b6c454b84a26eb0011e9aaf9bce4fd6f4f1e
kassandra-vasquez <kyanelyyy@gmail.com>
2024-08-02 11:05:05 -0700

    added optimized longest consequtive sequence solution

--- LongestConsequtiveSequence.py
+++ LongestConsequtiveSequence.py
@@ -19,3 +19,18 @@ class Solution:
                     longest = max(longest, curr_streak)
                     curr_streak = 1
         return max(longest, curr_streak)
+
+# OPTIMIZED
+# TC: T: O(n) S: O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest = 0
+        num_set = set(nums)
+
+        for n in num_set:
+            if (n - 1) not in num_set:
+                length = 1
+                while (n + length) in num_set:
+                    length += 1
+                longest = max(longest, length)
+        return longest
