7be4dc5161ac65ca6f805ee6e335fcda073176f0
kassandra-vasquez <kyanelyyy@gmail.com>
2024-08-05 18:49:38 -0700

    added optimized solution of product of array except self

--- ProductOfArrayExceptSelf.py
+++ ProductOfArrayExceptSelf.py
@@ -18,3 +18,23 @@ class Solution:
                         p *= nums[j]
                 res.append(p)
         return res
+
+
+# OPTIMIZED
+# TC: T: O(n) S: O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = []
+
+        if not 0 in nums:
+            d = prod(nums)
+            for i in nums:
+                res.append(int(d/i))
+        else:
+            for i in range(len(nums)):
+                p = 1
+                for j in range(len(nums)):
+                    if i != j:
+                        p *= nums[j]
+                res.append(p)
+        return res
